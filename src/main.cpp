f05b58e6a3faf646db94e9d1101b3163d679c7f4
Shourya Srivastava <ssgamingshourya@gmail.com>
2024-03-26 19:45:58 +0530

    This Main.cpp Is for Example for the Library

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,44 @@ SoftwareSerial BluetoorhSerial(3,4);
 void setup() {
   // put your setup code here, to run once:
   // Assign Servo Pins
-  Arm.BaseServoPin = 6;
-
+  Arm.BaseServoPin = 3;
+  Arm.HeightServoPin = 5;
+  Arm.MovementServoPin = 6;
+  Arm.ClawServoPin = 9;
 
 
   // Assign Default Positions
   Arm.BaseDefualtAngle = 90;
+  Arm.HeightDefualtAngle = 0;
+  Arm.MovementDefualtAngle = 90;
+  Arm.ClawDefualtAngle = 90;
 
 
   // Move To Deafault Positions, Using 1 interval to make it fast
   Arm.base.MoveTo(Arm.BaseDefualtAngle, 1);
-  
-  Arm.base.BaseServo.write(0);
+  Arm.height.MoveTo(Arm.HeightDefualtAngle, 1);
+  Arm.Movement.MoveTo(Arm.MovementDefualtAngle, 1);
+  Arm.claw.MoveTo(Arm.ClawDefualtAngle, 1);
+
+  // attach servos
   Arm.base.BaseServo.attach(Arm.BaseServoPin);
   Arm.height.HeightServo.attach(Arm.HeightServoPin);
   Arm.claw.ClawServo.attach(Arm.ClawServoPin);
   Arm.Movement.MovementServo.attach(Arm.MovementServoPin);
+  delay(500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Arm.base.MoveTo(0, 50);
+  Arm.base.MoveTo(0, 20);
+  Arm.Movement.MoveTo(140, 20);
+  Arm.claw.MoveTo(135, 20);
+  Arm.height.MoveTo(120, 20);
+  Arm.Movement.MoveTo(90, 20);
   delay(1000);
-  Arm.base.MoveTo(180, 50);
+  Arm.base.MoveTo(120, 20);
+  Arm.claw.MoveTo(90, 20);
+  Arm.height.MoveTo(0, 20);
   delay(1000);
-  
 
 }
